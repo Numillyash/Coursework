f47ca7c5dbf1dfcd8635be792c2d4d7862bc159e
Numillyash <ulanovsky.zhora@ya.ru>
2022-03-04 16:58:04 +0300

    Done a preform for RSA.c and RSA.h

--- Coursework/RSA.h
+++ Coursework/RSA.h
@@ -1,12 +1,7 @@
 #pragma once
 #include "config.h"
-#include "longArithmetic.h"
+#include "bit_LA.h"
 
-/// <summary>
-/// Функция для нахождения остатка от деления большого числа (число a в степени t) на малое
-/// </summary>
-/// <param name="a">Делимое</param>
-/// <param name="t">Делимое</param>
-/// <param name="b">Делитель</param>
-/// <returns>Остаток</returns>
-number module_pow(number* a, number* t, number* b);
+void generate_key(char* key_size_str, char* pubkey_filename, char* seckey_filename);
+
+void signify_file(char* input_filename, char* sec_code_filename, char* output_filename);
